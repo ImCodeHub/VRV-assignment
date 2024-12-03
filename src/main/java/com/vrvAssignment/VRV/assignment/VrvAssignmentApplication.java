67bb20622b48e806704d71a2ee10a55102a89a86
Ankit <ankitsharma.as420@gmail.com>
2024-12-03 17:38:19 +0530

    created project for spring security

---
+++ src/main/java/com/vrvAssignment/VRV/assignment/VrvAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.vrvAssignment.VRV.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrvAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrvAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrvAssignment/VRV/assignment/VrvAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrvAssignment.VRV.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrvAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
